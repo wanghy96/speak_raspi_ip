6595fae86624292bee5ed0d7cd443c6f9dc4bb8b
wanghy96 <33179834+wanghy96@users.noreply.github.com>
2019-10-28 22:49:04 +0800

    Update speak_ip.py

--- speak_ip.py
+++ speak_ip.py
@@ -8,7 +8,7 @@ import socket
 import subprocess
 
 voice_path = os.path.join(sys.path[0], 'voice')
-player = ["mplayer"，"omxplayer","mpg123","mpg321"]
+player = ["mplayer","omxplayer","mpg123","mpg321"]
 
 
 def getLocalIP():
