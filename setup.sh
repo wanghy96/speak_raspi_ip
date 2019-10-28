8fa00ecb2c2867417cbb079315c9a08d6df89a9b
wanghy96 <33179834+wanghy96@users.noreply.github.com>
2019-10-28 21:51:26 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ sudo apt-get install -y mplayer
 sudo git clone https://github.com/wanghy96/speak_raspi_ip.git
 sudo rm -rf /var/speak_raspi_ip/
 sudo mv speak_raspi_ip /var/
-echo 'sudo /usr/bin/python /var/speak_raspi_ip/speak_ip.py' | sudo tee -a /etc/rc.loca
+echo 'sudo /usr/bin/python /var/speak_raspi_ip/speak_ip.py' | sudo tee -a /etc/rc.local
 echo 'installed finished!'
