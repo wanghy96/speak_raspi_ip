d7ba573b64904064bb567e2034740cd49913d29c
wanghy96 <33179834+wanghy96@users.noreply.github.com>
2019-10-28 22:49:54 +0800

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo git clone https://github.com/wanghy96/speak_raspi_ip.git
-sudo rm -rf /var/speak_raspi_ip/
-sudo mv speak_raspi_ip/rc.local /etc/
-sudo chmod +x /etc/rc.local
-sudo mv speak_raspi_ip /var/
-echo 'finished!'
