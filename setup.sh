8b434db65397916c26857a598f48dfe0911eb95e
wanghy96 <33179834+wanghy96@users.noreply.github.com>
2019-11-01 06:51:36 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ sudo git clone https://github.com/wanghy96/speak_raspi_ip.git
 sudo apt-get install -y mpg123
 sudo amixer cset numid=3 1
 sudo rm -rf /var/speak_raspi_ip/
-sudo rm /etc/re.local
 sudo mv speak_raspi_ip/rc.local /etc/
 sudo chmod +x /etc/rc.local
 sudo mv speak_raspi_ip /var/
